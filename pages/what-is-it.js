6d2df30695ead38ab83a46ac1e0efc145f5ff580
Alain St. Pierre <alain@aspcanada.com>
2023-02-12 18:52:31 -0800

    WIP - remove template stuff

---
+++ pages/what-is-it.js
@@ -0,0 +1,12 @@
+// meta description
+// What is an Arm’s Length Mortgage? An “Arm’s Length Mortgage” refers to a 
+// mortgage that is held within an individual’s registered retirement…
+
+export default function WhatIsIt() {
+
+  return (
+    <div>
+      <h1>What is an Arm’s Length Mortgage?</h1>
+    </div>
+  )
+}
