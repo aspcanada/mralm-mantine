192b0dec4e5c1409042ad1ca8bec00714ef29228
Alain St. Pierre <alain@aspcanada.com>
2023-02-13 15:27:04 -0800

    fix more build issues

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,7 @@
 import Alert from './blog/alert'
-import Footer from '../components/footer'
-import Meta from '../components/meta'
-import Nav from '../components/nav'
+import Footer from './footer'
+import Meta from './meta'
+import Nav from './nav'
 
 export default function Layout({ preview, children }) {
   return (
