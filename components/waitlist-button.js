506da608a39b42720e3f3aaab7cb8699267d4712
Alain St. Pierre <alain@aspcanada.com>
2023-02-15 00:51:19 -0800

    fix modal styling

--- components/waitlist-button.js
+++ components/waitlist-button.js
@@ -91,7 +91,7 @@ export default function WaitlistButton() {
                   leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                 >
                   <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
-                    <div className="mt-12 xl:mt-0">
+                    <div>
                       <h3 className="text-base font-medium text-gray-900">
                         Join the waitlist
                       </h3>
