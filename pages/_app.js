36d6510ab0b88b9cc393d6cc78caf46ddca632ec
Alain St. Pierre <alain@aspcanada.com>
2023-03-09 14:46:38 -0800

    Add Tawk.to

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,17 @@
 import '../styles/index.css'
 import { Analytics } from '@vercel/analytics/react'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
       <Analytics />
-      {/* trigger deployment */}
+      {/* Tawk Widget */}
+      <Script
+        src="https://embed.tawk.to/64096f3f4247f20fefe4d4ac/1gr2dl422"
+        strategy="afterInteractive"
+      />
     </>
   )
 }
